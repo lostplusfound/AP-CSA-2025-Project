dc7587f129b13c324b223ffaebf3264ee21b45df
Ethan Effendi <ethaneffendi@outlook.com>
2025-03-14 19:05:24 +0000

    14 March 2025, 12:05 P.M.

--- src/Module.java
+++ src/Module.java
@@ -1,9 +1,14 @@
 public class Module {
     private int numQuestions;
+    private Difficulty difficulty; 
 
     public enum Difficulty {
         EASY, MEDIUM, HARD
     };
 
+    public Moddule(int numQuestions, Difficulty difficulty){
+        this.numQuestions = numQuestions; 
+        this.difficulty = difficulty; 
+    }
 
 }
