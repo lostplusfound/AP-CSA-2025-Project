1a91229998ee9a3a1980d5e431328cf6746cc7d9
coshpp <127819397+coshpp@users.noreply.github.com>
2025-03-18 12:19:42 -0700

    Update Module.java

--- src/Module.java
+++ src/Module.java
@@ -1,14 +1,26 @@
 public class Module {
-    private int numQuestions;
-    private Difficulty difficulty; 
+    private final String quizType; 
+    private final int numQuestions;
+    private final Difficulty difficulty;
 
+    // Enum for difficulty levels
     public enum Difficulty {
         EASY, MEDIUM, HARD
     };
 
-    public Module(int numQuestions, Difficulty difficulty){
-        this.numQuestions = numQuestions; 
-        this.difficulty = difficulty; 
+    // Constructor to initialize the module with number of questions and difficulty
+    public Module(String quizType, int numQuestions, Difficulty difficulty) {
+        this.quizType = quizType;
+        this.numQuestions = numQuestions;
+        this.difficulty = difficulty;
+    }
+
+    public int getNumQuestions() {
+        return numQuestions;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
     }
 
 }
